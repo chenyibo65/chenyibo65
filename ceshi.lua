14fc0c744024b37b07090b47b9ff225064d3ce0f
chenyibo65 <138267613+chenyibo65@users.noreply.github.com>
2024-12-04 22:42:59 +0800

    更新

---
+++ ceshi.lua
@@ -0,0 +1,247 @@
+--😡随便缝合下还看
+--Edited by BeeswarmMVP sub to my yt
+
+for _,v in pairs(game.workspace.Collectibles:GetChildren()) do
+
+if string.find(v.Name,"") then
+v:Destroy()
+end
+end
+ 
+local tuoidz = "Sunflower Field"
+local DarkCyberMainGUI = Instance.new("ScreenGui")
+local MiniPage = Instance.new("Frame")
+local Destroy = Instance.new("TextButton")
+local GameText = Instance.new("TextLabel")
+local MainPage = Instance.new("Frame")
+local VersionText = Instance.new("TextLabel")
+local HomePage = Instance.new("Frame")
+local ImageGang = Instance.new("ImageLabel")
+local CreditText = Instance.new("TextLabel")
+local JoinText = Instance.new("TextLabel")
+local DiscordlinkTest = Instance.new("TextLabel")
+local Copylinkdiscord = Instance.new("TextButton")
+local FarmingPage = Instance.new("Frame")
+local FieldSelectPage = Instance.new("ScrollingFrame")
+local SunflowerButton = Instance.new("TextButton")
+local MushroomButton = Instance.new("TextButton")
+local SpiderButton = Instance.new("TextButton")
+local BambooButton = Instance.new("TextButton")
+local RoseButton = Instance.new("TextButton")
+local PineTreeButton = Instance.new("TextButton")
+local CactusButton = Instance.new("TextButton")
+local PumpkinButton = Instance.new("TextButton")
+local PineappleButton = Instance.new("TextButton")
+local StumpButton = Instance.new("TextButton")
+local MountainTopButton = Instance.new("TextButton")
+local CoconutButton = Instance.new("TextButton")
+local PepperButton = Instance.new("TextButton")
+local StawberryButton = Instance.new("TextButton")
+local BlueflowerButton = Instance.new("TextButton")
+local DanrButton = Instance.new("TextButton")
+local DanrButton_2 = Instance.new("TextButton")
+local FieldSelectText = Instance.new("TextLabel")
+local HoneyText = Instance.new("TextLabel")
+local FieldText = Instance.new("TextLabel")
+local TimeElapsedText = Instance.new("TextLabel")
+local StartButton = Instance.new("TextButton")
+local FieldSelectText_2 = Instance.new("TextLabel")
+local WaypointPage = Instance.new("ScrollingFrame")
+local Suntp = Instance.new("TextButton")
+local Mushtp = Instance.new("TextButton")
+local Dantp = Instance.new("TextButton")
+local Blueftp = Instance.new("TextButton")
+local Clovertp = Instance.new("TextButton")
+local Bambootp = Instance.new("TextButton")
+local Spidertp = Instance.new("TextButton")
+local Strawtp = Instance.new("TextButton")
+local Castustp = Instance.new("TextButton")
+local Pumtp = Instance.new("TextButton")
+local Pintreetp = Instance.new("TextButton")
+local Rosetp = Instance.new("TextButton")
+local Toptp = Instance.new("TextButton")
+local Coconuttp = Instance.new("TextButton")
+local Stumptp = Instance.new("TextButton")
+local Pineappletp = Instance.new("TextButton")
+local Peppertp = Instance.new("TextButton")
+local SummonSprouttp = Instance.new("TextButton")
+local Coconutshoptp = Instance.new("TextButton")
+local Petaltp = Instance.new("TextButton")
+local Shopproctp = Instance.new("TextButton")
+local Shoptoptp = Instance.new("TextButton")
+local Shopmoontp = Instance.new("TextButton")
+local Shopbasictp = Instance.new("TextButton")
+local Shopsprinklertp = Instance.new("TextButton")
+local InstantConversiontp = Instance.new("TextButton")
+local MegaMemorytp = Instance.new("TextButton")
+local NightMemorytp = Instance.new("TextButton")
+local ExtremeMemorytp = Instance.new("TextButton")
+local StartHalltp = Instance.new("TextButton")
+local Anttp = Instance.new("TextButton")
+local Demonmasktp = Instance.new("TextButton")
+local TunelBeartp = Instance.new("TextButton")
+local WindyBeetp = Instance.new("TextButton")
+local Diamondmasktp = Instance.new("TextButton")
+local Soon = Instance.new("TextButton")
+local GummyMask = Instance.new("TextButton")
+local Afkstumptp = Instance.new("TextButton")
+local Stinger = Instance.new("TextButton")
+local Kingamulettp = Instance.new("TextButton")
+local ExtraPage = Instance.new("ScrollingFrame")
+local Autodig = Instance.new("TextButton")
+local Autobuffitem = Instance.new("TextButton")
+local Killcoconutcrab = Instance.new("TextButton")
+local KillTunnel = Instance.new("TextButton")
+local Usenightbell = Instance.new("TextButton")
+local Usefielddice = Instance.new("TextButton")
+local Autokillvicious = Instance.new("TextButton")
+local Autokillwindy = Instance.new("TextButton")
+local Autokillmonster = Instance.new("TextButton")
+local RemoteQuest = Instance.new("TextButton")
+local Autogumdrop = Instance.new("TextButton")
+local FindWindBee = Instance.new("TextButton")
+local Useglitter = Instance.new("TextButton")
+local Autogliter = Instance.new("TextButton")
+local Autococonut = Instance.new("TextButton")
+local Autostinger = Instance.new("TextButton")
+local Eqipdemonmask = Instance.new("TextButton")
+local Equipdemonmask = Instance.new("TextButton")
+local Equipgummymask = Instance.new("TextButton")
+local Automagicbean = Instance.new("TextButton")
+local Bringtornado = Instance.new("TextButton")
+local Killstumnail = Instance.new("TextButton")
+local Dropweapon = Instance.new("TextButton")
+local Noclip = Instance.new("TextButton")
+local AutoDispenser = Instance.new("TextButton")
+local Autotop = Instance.new("TextButton")
+local AutoDispenser_2 = Instance.new("TextButton")
+local Autored = Instance.new("TextButton")
+local Autoblue = Instance.new("TextButton")
+local ExtrasButton = Instance.new("TextButton")
+local FarmingButton = Instance.new("TextButton")
+local HomeButton = Instance.new("TextButton")
+local SettingButton = Instance.new("TextButton")
+local WaypointsButton = Instance.new("TextButton")
+local SettingPage = Instance.new("Frame")
+local Antiafk = Instance.new("TextLabel")
+local Antiafkbutton = Instance.new("TextButton")
+local Close = Instance.new("TextButton")
+local DarkCyberLoadingGUI = Instance.new("ScreenGui")
+local LoadingFrame = Instance.new("Frame")
+local Loadingimage = Instance.new("ImageLabel")
+local Createtitle = Instance.new("TextLabel")
+local NameTitle = Instance.new("TextLabel")
+local Loadingtext = Instance.new("TextLabel")
+local DarkCyberSynapshitGUI = Instance.new("ScreenGui")
+local SynapShitPage = Instance.new("Frame")
+local Createtitle = Instance.new("TextLabel")
+local NameTitle = Instance.new("TextLabel")
+local Welcome = Instance.new("TextLabel")
+ 
+--Properties:
+DarkCyberMainGUI.Name = "[DarkCyber]主界面"
+DarkCyberMainGUI.Parent = game.CoreGui
+ 
+MiniPage.Name = "迷你页"
+MiniPage.Parent = DarkCyberMainGUI
+MiniPage.BackgroundColor3 = Color3.new(0.321569, 0.462745, 1)
+MiniPage.BorderSizePixel = 0
+MiniPage.Position = UDim2.new(0.361979187, 0, -0.7, 0)
+MiniPage.Size = UDim2.new(0, 530, 0, 25)
+ 
+Destroy.Name = "摧毁"
+Destroy.Parent = MiniPage
+Destroy.BackgroundColor3 = Color3.new(1, 1, 1)
+Destroy.BackgroundTransparency = 1
+Destroy.Position = UDim2.new(0.950854659, 0, 0, 0)
+Destroy.Size = UDim2.new(0, 25, 0, 25)
+Destroy.Font = Enum.Font.GothamBold
+Destroy.Text = "X"
+Destroy.TextColor3 = Color3.new(1, 1, 1)
+Destroy.TextSize = 20
+ 
+GameText.Name = "游戏文本"
+GameText.Parent = MiniPage
+GameText.BackgroundColor3 = Color3.new(1, 1, 1)
+GameText.BackgroundTransparency = 1
+GameText.Position = UDim2.new(0.337588102, 0, -0.0399999991, 0)
+GameText.Size = UDim2.new(0.324587107, 0, 0.999999702, 0)
+GameText.Font = Enum.Font.GothamBold
+GameText.Text = "蜂群模拟器"
+GameText.TextColor3 = Color3.new(1, 1, 1)
+GameText.TextSize = 16
+ 
+MainPage.Name = "主页面"
+MainPage.Parent = MiniPage
+MainPage.Active = true
+MainPage.BackgroundColor3 = Color3.new(0.321569, 0.462745, 1)
+MainPage.BorderSizePixel = 0
+MainPage.Position = UDim2.new(0, 0, 1.0016675, 0)
+MainPage.Selectable = true
+MainPage.Size = UDim2.new(1, 0, 8.9163208, 0)
+ 
+VersionText.Name = "版本文本"
+VersionText.Parent = MainPage
+VersionText.BackgroundColor3 = Color3.new(1, 1, 1)
+VersionText.BackgroundTransparency = 1
+VersionText.Position = UDim2.new(-0.00158959627, 0, 0.914668798, 0)
+VersionText.Size = UDim2.new(0.213481352, 0, 0.08324752, 0)
+VersionText.Font = Enum.Font.GothamBold
+VersionText.Text = "版本: 1.0.0"
+VersionText.TextColor3 = Color3.new(1, 1, 1)
+VersionText.TextSize = 16
+ 
+HomePage.Name = "主页"
+HomePage.Parent = MainPage
+HomePage.BackgroundColor3 = Color3.new(1, 1, 1)
+HomePage.BackgroundTransparency = 1
+HomePage.BorderSizePixel = 0
+HomePage.Position = UDim2.new(0, 0, 0.132234111, 0)
+HomePage.Size = UDim2.new(1, 0, 0.782434821, 0)
+ 
+ImageGang.Name = "ImageGang公司"
+ImageGang.Parent = HomePage
+ImageGang.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageGang.Position = UDim2.new(0.0295857992, 0, 0.0601093024, 0)
+ImageGang.Size = UDim2.new(0, 70, 0, 70)
+ImageGang.Image = "http://www.roblox.com/asset/?id=3766691866"
+ 
+CreditText.Name = "创建文本"
+CreditText.Parent = HomePage
+CreditText.BackgroundColor3 = Color3.new(1, 1, 1)
+CreditText.BackgroundTransparency = 1
+CreditText.Position = UDim2.new(0.188195452, 0, 0.0601093657, 0)
+CreditText.Size = UDim2.new(0.809829473, 0, 0.382513672, 0)
+CreditText.Font = Enum.Font.GothamBold
+CreditText.Text = "Gui 创建 : DarkCyber"
+CreditText.TextColor3 = Color3.new(1, 1, 1)
+CreditText.TextSize = 20
+ 
+JoinText.Name = "链接文本"
+JoinText.Parent = HomePage
+JoinText.BackgroundColor3 = Color3.new(1, 1, 1)
+JoinText.BackgroundTransparency = 1
+JoinText.Position = UDim2.new(0, 0, 0.453551859, 0)
+JoinText.Size = UDim2.new(0.998024881, 0, 0.146612123, 0)
+JoinText.Font = Enum.Font.GothamBold
+JoinText.Text = "加入出生们的q群吧"
+JoinText.TextColor3 = Color3.new(1, 1, 1)
+JoinText.TextSize = 17
+ 
+DiscordlinkTest.Name = "在这里加入"
+DiscordlinkTest.Parent = HomePage
+DiscordlinkTest.BackgroundColor3 = Color3.new(1, 1, 1)
+DiscordlinkTest.BackgroundTransparency = 1
+DiscordlinkTest.Position = UDim2.new(0, 0, 0.601092875, 0)
+DiscordlinkTest.Size = UDim2.new(0.998024881, 0, 0.146612123, 0)
+DiscordlinkTest.Font = Enum.Font.GothamBold
+DiscordlinkTest.Text = "q群：764452461"
+DiscordlinkTest.TextColor3 = Color3.new(1, 1, 1)
+DiscordlinkTest.TextSize = 17
+ 
+FarmingPage.Name = "花粉界面"
+FarmingPage.Parent = MainPage
+FarmingPage.BackgroundColor3 = Color3.new(1, 1, 1)
+FarmingPage.BackgroundTransparency = 1
+FarmingPage.Position = UDim2.new(-5.75803369e-08, 0, 0.127000019, 0)
+FarmingPage.Size = UDim2.new(0.999777615, 0, 0.787668824, 
